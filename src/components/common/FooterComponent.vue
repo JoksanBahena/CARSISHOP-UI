2a70a2502b4a3cc291a1621b4cf0816c3a8bd3b5
CrisSoven <20213tn044@utez.edu.mx>
2024-03-13 09:55:46 -0600

    [ADD] definición de layouts principales

---
+++ src/components/common/FooterComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div
+      class="px-4 py-2 text-center w-100"
+      :style="{ background: colors.primary, color: colors.primary_dark }"
+    >
+      <strong> {{ new Date().getFullYear() }} — CarsiShop </strong>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Colors from "@/utils/Colors";
+
+const colors = {
+  primary: Colors.cs_primary,
+  primary_dark: Colors.cs_primary_dark,
+  secondary: Colors.cs_secondary,
+};
+</script>
---
+++ src/layouts/user/DefaultLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-screen">
+    <navbar-component />
+    <main class="ma-4">
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import NavbarComponent from "@/components/common/NavbarComponent.vue";
+import FooterComponent from "@/components/common/FooterComponent.vue";
+</script>
