f0dc4a46068765b1ee9534b6388401c5fa3c9fd0
BetjaderOD <20213tn032@utez.edu.mx>
2024-03-18 18:42:04 -0600

    [ADD] Maquetado pedidos y devoluciones

--- src/views/profile/ProfileOrdersReturnsView.vue
+++ src/views/profile/ProfileOrdersReturnsView.vue
@@ -1,110 +1,77 @@
 <template>
   <profile-layout>
-    <v-breadcrumbs :items="['Inicio', 'Mi perfil', 'Mi Cuenta']"></v-breadcrumbs>
-    <h1 class='ml-5'>Mi cuenta</h1>
+    <v-breadcrumbs :items="['Inicio', 'Mi perfil', 'Pedidos y devoluciones']"></v-breadcrumbs>
+    <h1 class='ml-5'>Pedidos y devoluciones</h1>
+
+   <v-row class="ml-1 mt-6 pl-4">
+      <div class="col-auto mr-4 cursor-pointer" @click="step = 1">
+        <h3 class="font-weight-black">Pedidos</h3>
+        <div  :class="{ 'line': step === 1 }"/>
+      </div>
+      <div class="col-auto cursor-pointer" @click="step = 2">
+        <h3 class="font-weight-black">Devoluciones</h3>
+        <div  :class="{ 'line': step === 2 }"/>
+      </div>
+    </v-row>
 
     <v-container>
-      <v-row>
-        <v-col cols="12" md="4">
-          <v-img lazy-src="@/assets/imgs/men_card.webp" src="@/assets/imgs/men_card.webp" width="200" height="200"
-            aspect-ratio="1" class="bg-grey-lighten-2 rounded-circle" cover>
-          </v-img>
-        </v-col>
-        <v-col cols="12" md="4" class="mt-auto">
-          <div class="mb-4">
-            <div class="text-subtitle-1 font-weight-medium">
-              Nombre(s)
+      <v-window v-model="step">
+        <v-window-item :value="1">
+          <v-row class="pl-7 mt-10 " rows="12" md="4">
+    <v-card variant="flat" width="100%">
+          <v-card-item>
+            <v-card-title class="mb-3">TIENDA / VENDEDOR</v-card-title>
+            <V-divider></v-divider>
+            <div class=" mt-3 d-flex justify-content-end align-items-center">
+              <v-icon class="mdi mdi-close ml-auto"></v-icon>
             </div>
-            <v-text-field density="compact" placeholder="Nombre(s)" prepend-inner-icon="mdi-account-outline"
-              variant="outlined" />
-          </div>
-        </v-col>
-        <v-col cols="12" md="4" class="mt-auto">
-          <div class="mb-4">
-            <div class="text-subtitle-1 font-weight-medium">
-              Apellidos
-            </div>
-            <v-text-field density="compact" placeholder="Apellidos" prepend-inner-icon="mdi-account-outline"
-              variant="outlined" />
-          </div>
-        </v-col>
+            <v-row>
+              <v-col>
+                <v-card-item>
+                  <template v-slot:prepend>
+                    <v-img src="@/assets/imgs/item.webp" width="100" height="100" class="mr-2"></v-img>
+                  </template>
+                  <v-card-title class="mb-3 ml-4">ITEM</v-card-title>
+                  <v-card-text class="py-0 mb-4">Descriccion</v-card-text>
+                  <v-card-text class="py-1">Option</v-card-text>
+                </v-card-item>
+              </v-col>
+              <v-col>
+                <v-card-actions class="flex-column">
+                  <v-btn variant="outlined" class="mb-8 text-none" :color="colors.primary_dark">
+                    Detalles de compra
+                  </v-btn>
+                  <v-btn variant="outlined" class="mb-8 text-none" :color="colors.primary_dark">
+                    Solicitar devolución
+                  </v-btn>
+                </v-card-actions>
+              </v-col>
 
-      </v-row>
+            </v-row>
 
-      <v-row>
-        <v-col cols="12" md="4">
-          <div class="mb-4">
-            <div class="text-subtitle-1 font-weight-medium">
-              Fecha de nacimiento
-            </div>
-            <v-text-field density="compact" placeholder="Correo electrónico"
-              prepend-inner-icon="mdi-calendar-month-outline" variant="outlined" />
-          </div>
-        </v-col>
-        <v-col cols="12" md="4">
-          <div class="mb-4">
-            <div class="text-subtitle-1 font-weight-medium">
-              Genero
-            </div>
-            <v-text-field density="compact" placeholder="Genero" prepend-inner-icon="mdi-account-outline"
-              variant="outlined" />
-          </div>
+          </v-card-item>
+        </v-card>
+                </v-row>
+        </v-window-item>
 
-        </v-col>
-        <v-col cols="12" md="4">
-          <div class="mb-4">
-            <div class="text-subtitle-1 font-weight-medium">
-              Telefono
-            </div>
-            <v-text-field density="compact" placeholder="Telefono" prepend-inner-icon="mdi-phone-outline"
-              variant="outlined" />
-          </div>
-        </v-col>
+        <v-window-item :value="2">
 
-      </v-row>
-      <v-row>
-
-
-        <v-col cols="12" md="4">
-          <div class="mb-4">
-            <div class="text-subtitle-1 font-weight-medium">
-              Dirección
-            </div>
-            <v-text-field density="compact" placeholder="Correo electrónico" prepend-inner-icon="mdi-google-maps"
-              variant="outlined" />
-          </div>
-        </v-col>
-        <v-col cols="12" md="4">
-          <div class="text-subtitle-1 font-weight-medium">
-            Correo
-          </div>
-          <v-text-field density="compact" placeholder="Correo electrónico" prepend-inner-icon="mdi-email-outline"
-            variant="outlined" />
-        </v-col>
-        <v-col cols="12" md="4">
-          <div class="text-subtitle-1 font-weight-medium">
-            Foto de perfil
-          </div>
-          <v-file-input density="compact" type="file" accept="image/png, image/jpeg, image/bmp" variant="outlined"
-            prepend-icon="mdi-camera-outline" chips show-size @change="onFileChange" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <v-btn class="mb-8 text-none" :color="colors.primary_dark" size="large" block>
-            Editar
-          </v-btn>
-        </v-col>
-      </v-row>
+         <v-container class="d-flex align-center justify-center flex-column">
+      <v-icon class='mdi mdi-cart-outline ' size="x-large"></v-icon>
+      <h2 class="text-center mt-4">Actualmente no tienes ninguna devolución</h2>
+      <p class="text-center mt-4">Puedes iniciar una devolución desde el seguimiento de tu pedido</p>
+      <v-btn  :style="{ 'border-color': colors.primary, 'color':colors.primary_dark }" variant='outlined' class="mt-5"   @click="step = 1">Pedidos</v-btn>
     </v-container>
-
+        </v-window-item>
+      </v-window>
+    </v-container>
   </profile-layout>
 </template>
 
 <script setup>
 import Colors from "@/utils/Colors.js";
-import { ref } from "vue";
 import ProfileLayout from "@/layouts/user/ProfileLayout.vue";
+import { ref } from "vue";
 
 const colors = {
   primary: Colors.cs_primary,
@@ -112,16 +79,14 @@ const colors = {
   white: Colors.cs_white,
 };
 
-const image_url = ref("");
-
-const onFileChange = (e) => {
-  const file = e.target.files[0];
-  const reader = new FileReader();
-
-  reader.onload = (e) => {
-    image_url.value = e.target.result;
-  };
-
-  reader.readAsDataURL(file);
-};
+let step = ref(1);
 </script>
+
+<style scoped>
+.line {
+  border: 1px solid black;
+}
+a {
+  color: black;
+}
+</style>
