932ef0a1647168492268725deedf5aef819fc71b
CrisSoven <20213tn044@utez.edu.mx>
2024-03-21 00:09:10 -0600

    [MODIFY] diseño nuevo de la lista y card de productos

--- src/components/common/ProductCardComponent.vue
+++ src/components/common/ProductCardComponent.vue
@@ -1,33 +1,36 @@
 <template>
   <v-card
     class="mr-2"
-    max-width="326"
     variant="flat"
+    width="300"
+    height="500"
     :color="colors.secondary"
     href="/product/1"
   >
-    <v-img height="200px" :src="item.image"></v-img>
+    <v-img class="my-4" height="300" src="@/assets/imgs/item.webp" />
 
     <v-card-title>
       {{ item.title }}
     </v-card-title>
-    <p class="mx-4" :style="{ lineBreak: 'auto' }">
+
+    <p class="mx-4">
       {{ item.description }}
     </p>
 
-    <v-card-actions>
-      <p class="font-weight-medium text-h5">{{ item.price }}</p>
-      <v-spacer></v-spacer>
+    <v-card-actions class="align-end">
+      <p class="font-weight-medium text-h5 mr-2">{{ item.price }}</p>
+      <p class="font-weight-medium text-subtitle-2">{{ item.price }}</p>
+      <v-spacer />
       <v-btn variant="outlined" :style="{ borderColor: colors.primary_dark }">
         <v-icon
           :color="colors.primary_dark"
           class="text-h5"
           icon="mdi-cart-outline"
-        ></v-icon>
+        />
       </v-btn>
     </v-card-actions>
   </v-card>
-</template> 
+</template>
 
 <script setup>
 import Colors from "@/utils/Colors.js";
@@ -42,9 +45,9 @@ const colors = {
 const item = {
   id: 1,
   title: "Item 1",
-  description: "Lorem ipsum dolor sit amet consectetur.",
+  description: "Lorem ipsum dolor sit amet consectetur. ",
   price: "$615 MX",
+  priceDiscount: "$500 MX",
   image: "@/assets/imgs/item.webp",
 };
-
 </script>
--- src/components/common/ProductListComponent.vue
+++ src/components/common/ProductListComponent.vue
@@ -1,72 +1,19 @@
 <template>
-  <div class="mt-10">
+  <v-sheet class="mt-12">
     <h2>{{ title }}</h2>
-    <v-sheet class="mt-5">
-      <v-slide-group
-        v-model="model"
-        class="pa-4"
-        selected-class="bg-success"
-        show-arrows
-      >
-        <v-slide-group-item v-for="n in 15" :key="n">
-          <product-card-component />
-        </v-slide-group-item>
-      </v-slide-group>
-    </v-sheet>
-  </div>
-</template> 
+    <v-slide-group v-model="model" class="pa-4" show-arrows>
+      <v-slide-group-item v-for="n in 15" :key="n">
+        <product-card-component />
+      </v-slide-group-item>
+    </v-slide-group>
+  </v-sheet>
+</template>
 
 <script setup>
 import ProductCardComponent from "@/components/common/ProductCardComponent.vue";
 import { ref } from "vue";
 
 const model = ref(null);
-import Colors from "@/utils/Colors.js";
-
-const colors = {
-  primary: Colors.cs_primary,
-  primary_dark: Colors.cs_primary_dark,
-  white: Colors.cs_white,
-  secondary: Colors.cs_secondary,
-};
-
-const items = [
-  {
-    id: 1,
-    title: "Item 1",
-    description: "Lorem ipsum dolor sit amet consectetur.",
-    price: "$615 MX",
-    image: "@/assets/imgs/item.webp",
-  },
-  {
-    id: 2,
-    title: "Item 2",
-    description: "Lorem ipsum dolor sit amet consectetur.",
-    price: "$615 MX",
-    image: "@/assets/imgs/item.webp",
-  },
-  {
-    id: 3,
-    title: "Item 3",
-    description: "Lorem ipsum dolor sit amet consectetur.",
-    price: "$615 MX",
-    image: "@/assets/imgs/item.webp",
-  },
-  {
-    id: 4,
-    title: "Item 4",
-    description: "Lorem ipsum dolor sit amet consectetur.",
-    price: "$615 MX",
-    image: "@/assets/imgs/item.webp",
-  },
-  {
-    id: 4,
-    title: "Item 4",
-    description: "Lorem ipsum dolor sit amet consectetur.",
-    price: "$615 MX",
-    image: "@/assets/imgs/item.webp",
-  },
-];
 
 const props = defineProps({
   title: {
