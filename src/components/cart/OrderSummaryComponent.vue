ea83f7f1c2395a43e09a9043e63a10b749a30cb6
CrisSoven <20213tn044@utez.edu.mx>
2024-03-25 00:36:52 -0600

    [ADD] fragmentación de componentes de cart-module

---
+++ src/components/cart/OrderSummaryComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card variant="flat">
+    <v-divider class="mb-4 d-flex d-md-none" />
+    <v-card-title> Detalles de la compra (19 productos) </v-card-title>
+    <v-card-item class="mb-4">
+      <div class="d-flex flex-fow text-subtitle-1 mt-2">
+        <p>Subtotal</p>
+        <p class="font-weight-bold ml-auto">{{ subtotal }}</p>
+      </div>
+      <div class="d-flex flex-row text-subtitle-1 mt-2">
+        <p>Descuento</p>
+        <p class="font-weight-bold ml-auto">{{ discount }}</p>
+      </div>
+      <div class="d-flex flex-row text-subtitle-1 mt-2">
+        <p>Total</p>
+        <p class="font-weight-bold ml-auto">{{ total }}</p>
+      </div>
+    </v-card-item>
+    <v-divider />
+  </v-card>
+</template>
+
+<script setup>
+const props = defineProps({
+  subtotal: {
+    type: String,
+    default: "$0.00 MX",
+  },
+  discount: {
+    type: String,
+    default: "$0.00 MX",
+  },
+  total: {
+    type: String,
+    default: "$0.00 MX",
+  },
+});
+</script>
---
+++ src/components/cart/PaymentMethodComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card variant="flat" class="mt-4">
+    <v-card-title>Método de pago</v-card-title>
+    <v-card-item>
+      <template v-slot:prepend>
+        <v-img
+          src="@/assets/imgs/mc.webp"
+          width="50"
+          height="50"
+          class="mr-2"
+        />
+      </template>
+      <div class="d-flex flex-fow text-subtitle-1">
+        <p>{{ user }}</p>
+      </div>
+      <div class="d-flex flex-fow text-subtitle-1">
+        <p>•••• •••• •••• {{ last_four }}</p>
+      </div>
+    </v-card-item>
+  </v-card>
+</template>
+
+<script setup>
+const props = defineProps({
+  user: {
+    type: String,
+    default: "Dueño de la tarjeta",
+  },
+  last_four: {
+    type: String,
+    default: "1234",
+  },
+});
+</script>
