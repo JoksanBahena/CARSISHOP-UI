26b68507f3bafa9a1b346341a86bde63f1e9b32c
BetjaderOD <20213tn032@utez.edu.mx>
2024-03-26 12:47:23 -0600

    [MODIFY] Funcion de cambiar el estado de edicion

--- src/views/profile/ProfileAccountView.vue
+++ src/views/profile/ProfileAccountView.vue
@@ -6,23 +6,39 @@
       <v-form>
         <v-row>
           <v-col cols="12" md="4">
-            <v-img lazy-src="@/assets/imgs/men_card.webp" src="@/assets/imgs/men_card.webp" width="200" height="200"
-              aspect-ratio="1" class="-lighten-2 rounded-circle" cover>
+            <v-img
+              lazy-src="@/assets/imgs/men_card.webp"
+              src="@/assets/imgs/men_card.webp"
+              width="200"
+              height="200"
+              aspect-ratio="1"
+              class="-lighten-2 rounded-circle"
+              cover
+            >
             </v-img>
           </v-col>
           <v-col cols="12" md="4" class="mt-auto">
             <div class="mb-4">
               <div class="text-subtitle-1 font-weight-medium">Nombre(s)</div>
-              <v-text-field density="compact" placeholder="Nombre(s)" prepend-inner-icon="mdi-account-outline"
-                variant="outlined" :disabled="is_disabled" />
-
+              <v-text-field
+                density="compact"
+                placeholder="Nombre(s)"
+                prepend-inner-icon="mdi-account-outline"
+                variant="outlined"
+                :disabled="is_disabled"
+              />
             </div>
           </v-col>
           <v-col cols="12" md="4" class="mt-auto">
             <div class="mb-4">
               <div class="text-subtitle-1 font-weight-medium">Apellidos</div>
-              <v-text-field density="compact" placeholder="Apellidos" prepend-inner-icon="mdi-account-outline"
-                variant="outlined" :disabled="is_disabled" />
+              <v-text-field
+                density="compact"
+                placeholder="Apellidos"
+                prepend-inner-icon="mdi-account-outline"
+                variant="outlined"
+                :disabled="is_disabled"
+              />
             </div>
           </v-col>
         </v-row>
@@ -33,22 +49,37 @@
               <div class="text-subtitle-1 font-weight-medium">
                 Fecha de nacimiento
               </div>
-              <v-text-field density="compact" placeholder="Correo electrónico"
-                prepend-inner-icon="mdi-calendar-month-outline" variant="outlined" :disabled="is_disabled" />
+              <v-text-field
+                density="compact"
+                placeholder="Correo electrónico"
+                prepend-inner-icon="mdi-calendar-month-outline"
+                variant="outlined"
+                :disabled="is_disabled"
+              />
             </div>
           </v-col>
           <v-col cols="12" md="4">
             <div class="mb-4">
               <div class="text-subtitle-1 font-weight-medium">Genero</div>
-              <v-text-field density="compact" placeholder="Genero" prepend-inner-icon="mdi-account-outline"
-                variant="outlined" :disabled="is_disabled" />
+              <v-text-field
+                density="compact"
+                placeholder="Genero"
+                prepend-inner-icon="mdi-account-outline"
+                variant="outlined"
+                :disabled="is_disabled"
+              />
             </div>
           </v-col>
           <v-col cols="12" md="4">
             <div class="mb-4">
               <div class="text-subtitle-1 font-weight-medium">Telefono</div>
-              <v-text-field density="compact" placeholder="Telefono" prepend-inner-icon="mdi-phone-outline"
-                variant="outlined" :disabled="is_disabled" />
+              <v-text-field
+                density="compact"
+                placeholder="Telefono"
+                prepend-inner-icon="mdi-phone-outline"
+                variant="outlined"
+                :disabled="is_disabled"
+              />
             </div>
           </v-col>
         </v-row>
@@ -56,30 +87,55 @@
           <v-col cols="12" md="4">
             <div class="mb-4">
               <div class="text-subtitle-1 font-weight-medium">Dirección</div>
-              <v-text-field density="compact" placeholder="Correo electrónico" prepend-inner-icon="mdi-google-maps"
-                variant="outlined" :disabled="is_disabled" />
+              <v-text-field
+                density="compact"
+                placeholder="Correo electrónico"
+                prepend-inner-icon="mdi-google-maps"
+                variant="outlined"
+                :disabled="is_disabled"
+              />
             </div>
           </v-col>
           <v-col cols="12" md="4">
             <div class="text-subtitle-1 font-weight-medium">Correo</div>
-            <v-text-field density="compact" placeholder="Correo electrónico" prepend-inner-icon="mdi-email-outline"
-              variant="outlined" :disabled="is_disabled" />
+            <v-text-field
+              density="compact"
+              placeholder="Correo electrónico"
+              prepend-inner-icon="mdi-email-outline"
+              variant="outlined"
+              :disabled="is_disabled"
+            />
           </v-col>
           <v-col cols="12" md="4">
             <div class="text-subtitle-1 font-weight-medium">Foto de perfil</div>
-            <v-file-input density="compact" type="file" accept="image/png, image/jpeg, image/bmp" variant="outlined"
-              prepend-icon="mdi-camera-outline" chips show-size @change="onFileChange" :disabled="is_disabled" />
+            <v-file-input
+              density="compact"
+              type="file"
+              accept="image/png, image/jpeg, image/bmp"
+              variant="outlined"
+              prepend-icon="mdi-camera-outline"
+              chips
+              show-size
+              @change="onFileChange"
+              :disabled="is_disabled"
+            />
           </v-col>
         </v-row>
         <v-row>
           <v-divider></v-divider>
 
           <v-col cols="12">
-            <v-btn variant="outlined" prepend-icon="mdi-pencil" class="text-none" :color="colors.primary_dark"
-              size="large" block @click="is_disabled ? onEdit() : onSave()">
+            <v-btn
+              variant="outlined"
+              prepend-icon="mdi-pencil"
+              class="text-none"
+              :color="colors.primary_dark"
+              size="large"
+              block
+              @click="is_disabled ? onEdit() : onSave()"
+            >
               {{ is_disabled ? "Editar" : "Guardar" }}
             </v-btn>
-
           </v-col>
         </v-row>
       </v-form>
@@ -91,8 +147,16 @@
 import Colors from "@/utils/Colors.js";
 import { ref } from "vue";
 import ProfileLayout from "@/layouts/user/ProfileLayout.vue";
-import { useVuelidate } from '@vuelidate/core';
-import { required } from '@vuelidate/validators';
+import { useVuelidate } from "@vuelidate/core";
+import {
+  required,
+  integer,
+  minLength,
+  maxLength,
+  helpers,
+} from "@vuelidate/validators";
+
+const { withMessage, regex } = helpers;
 
 const items = [
   {
@@ -133,6 +197,11 @@ const onFileChange = (e) => {
 
 const is_disabled = ref(true);
 
+const onEdit = () => {
+  is_disabled.value = false;
+};
 
-
+const onSave = () => {
+  is_disabled.value = true;
+};
 </script>
