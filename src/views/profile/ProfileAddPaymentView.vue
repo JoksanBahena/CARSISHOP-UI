a9f36c3b81deebd95a7e78343e9f8cc7dd8950e2
CrisSoven <20213tn044@utez.edu.mx>
2024-03-26 02:47:35 -0600

    [ADD] validaciones para agregar una tarjeta

--- src/views/profile/ProfileAddPaymentView.vue
+++ src/views/profile/ProfileAddPaymentView.vue
@@ -1,137 +1,241 @@
 <template>
   <profile-layout>
     <v-breadcrumbs :items="items"></v-breadcrumbs>
-    <h1 class='ml-5'>Metodos de pagos</h1>
 
     <v-container>
-      <h4>Datos bancarios</h4>
-      <v-form>
+      <p class="text-h4 font-weight-medium mb-2">Métodos de pago</p>
 
-      <v-row class="mt-5">
-        <v-col cols="8" md="4">
-          <div class="mb-4">
-            <div class="text-subtitle-1 font-weight-medium">
-              Nombre(s)
-            </div>
-            <v-text-field
-              density="compact"
-              placeholder="Nombre(s)"
-              prepend-inner-icon="mdi-account-outline"
-              variant="outlined"
-            />
-          </div>
-        </v-col>
-        <v-col cols="8" md="4">
-          <div class="mb-4">
-            <div class="text-subtitle-1 font-weight-medium">
-              Apellidos
-            </div>
-            <v-text-field
-              density="compact"
-              placeholder="Apellidos"
-              prepend-inner-icon="mdi-account-outline"
-              variant="outlined"
-            />
-          </div>
-        </v-col>
-
-      </v-row>
-      <v-row class="mt-5">
-        <v-col cols="8">
-          <div class="mb-4">
-            <div class="text-subtitle-1 font-weight-medium">
-              Numero de tarjeta
-            </div>
-            <v-text-field
-              density="compact"
-              placeholder="Numero de tarjeta"
-              prepend-inner-icon="mdi-account-outline"
-              variant="outlined"
-            />
-          </div>
-        </v-col>
-      </v-row>
-      <v-row class="mt-5">
-        <v-col cols="8" md="4">
-          <div class="mb-4">
-            <div class="text-subtitle-1 font-weight-medium">
-              Fecha de vencimiento
-            </div>
-            <v-text-field
-              density="compact"
-              placeholder="Fecha de vencimiento"
-              prepend-inner-icon="mdi-account-outline"
-              variant="outlined"
-            />
-          </div>
-        </v-col>
-        <v-col cols="8" md="4">
-          <div class="mb-4">
-            <div class="text-subtitle-1 font-weight-medium">
-              CCV
-            </div>
-            <v-text-field
-              density="compact"
-              placeholder="CCV"
-              prepend-inner-icon="mdi-account-outline"
-              variant="outlined"
-            />
-          </div>
-        </v-col>
-
-      </v-row>
-      <v-row>
-        <v-col cols="8">
-          <v-btn
-            variant="outlined"
-            class="mb-8  text-none"
-            :color="colors.primary_dark"
-            block
-            href="/profilePayment"
-            prepend-icon="mdi-plus"
-          >Guardar</v-btn>
-        </v-col>
-      </v-row>
-            </v-form>
-
+      <v-card variant="flat" class="mt-4">
+        <v-card-title>Datos bancarios</v-card-title>
+        <v-card-item>
+          <v-form>
+            <v-row>
+              <v-col cols="12" lg="9">
+                <div>
+                  <div class="text-subtitle-1 font-weight-medium">
+                    Nombre en la tarjeta
+                  </div>
+                  <v-text-field
+                    v-model="state.owner"
+                    density="compact"
+                    placeholder="Nombre en la tarjeta"
+                    prepend-inner-icon="mdi-account-outline"
+                    variant="outlined"
+                    @blur="v$.owner.$touch"
+                    @input="v$.owner.$touch"
+                    :error-messages="v$.owner.$errors.map((e) => e.$message)"
+                  />
+                </div>
+              </v-col>
+              <v-col cols="12" lg="9">
+                <div>
+                  <div class="text-subtitle-1 font-weight-medium">
+                    Numero de tarjeta
+                  </div>
+                  <v-text-field
+                    v-model="state.number"
+                    density="compact"
+                    placeholder="Numero de tarjeta"
+                    prepend-inner-icon="mdi-account-outline"
+                    variant="outlined"
+                    :counter="16"
+                    @blur="v$.number.$touch"
+                    @input="v$.number.$touch"
+                    :error-messages="v$.number.$errors.map((e) => e.$message)"
+                  />
+                </div>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="6" lg="3">
+                <div>
+                  <div class="text-subtitle-1 font-weight-medium">
+                    Mes de vencimiento
+                  </div>
+                  <v-select
+                    v-model="state.expiration_month"
+                    density="compact"
+                    placeholder="Mes de vencimiento"
+                    prepend-inner-icon="mdi-account-outline"
+                    variant="outlined"
+                    :items="months"
+                    @blur="v$.expiration_month.$touch"
+                    @input="v$.expiration_month.$touch"
+                    :error-messages="
+                      v$.expiration_month.$errors.map((e) => e.$message)
+                    "
+                  />
+                </div>
+              </v-col>
+              <v-col cols="6" lg="3">
+                <div>
+                  <div class="text-subtitle-1 font-weight-medium">
+                    Año de vencimiento
+                  </div>
+                  <v-select
+                    v-model="state.expiration_year"
+                    density="compact"
+                    placeholder="Año de vencimiento"
+                    prepend-inner-icon="mdi-account-outline"
+                    variant="outlined"
+                    :items="years"
+                    @blur="v$.expiration_year.$touch"
+                    @input="v$.expiration_year.$touch"
+                    :error-messages="
+                      v$.expiration_year.$errors.map((e) => e.$message)
+                    "
+                  />
+                </div>
+              </v-col>
+              <v-col cols="12" lg="3">
+                <div>
+                  <div class="text-subtitle-1 font-weight-medium">CCV</div>
+                  <v-text-field
+                    v-model="state.cvv"
+                    density="compact"
+                    placeholder="CCV"
+                    variant="outlined"
+                    :counter="3"
+                    :append-inner-icon="
+                      visible ? 'mdi-eye-off-outline' : 'mdi-eye-outline'
+                    "
+                    :type="visible ? 'text' : 'password'"
+                    @click:append-inner="visible = !visible"
+                    @blur="v$.cvv.$touch"
+                    @input="v$.cvv.$touch"
+                    :error-messages="v$.cvv.$errors.map((e) => e.$message)"
+                  />
+                </div>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" lg="9">
+                <v-btn
+                  variant="flat"
+                  class="text-none"
+                  :color="colors.primary_dark"
+                  block
+                  append-icon="mdi-check-circle-outline"
+                  @click="submitForm()"
+                >
+                  Guardar
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-card-item>
+      </v-card>
     </v-container>
-
   </profile-layout>
 </template>
 
 <script setup>
 import Colors from "@/utils/Colors.js";
-import { ref } from "vue";
 import ProfileLayout from "@/layouts/user/ProfileLayout.vue";
 
- const items = [
-  {
-    title: 'Inicio',
-    disabled: false,
-    href: '/',
-  },
-  {
-    title: 'Perfil',
-    disabled: false,
-    href: '/profileAccount',
-  },
-  {
-    title: 'Metodos de pago',
-    disabled: false,
-    href: '/profilePayment',
+import { ref } from "vue";
+import { reactive } from "vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required, minLength, maxLength, helpers } from "@vuelidate/validators";
 
+const { withMessage, regex } = helpers;
+
+const items = [
+  {
+    title: "Inicio",
+    disabled: false,
+    href: "/",
   },
   {
-    title: 'Agregar metodo de pago',
+    title: "Perfil",
+    disabled: false,
+    href: "/profileAccount",
+  },
+  {
+    title: "Metodos de pago",
+    disabled: false,
+    href: "/profilePayment",
+  },
+  {
+    title: "Agregar metodo de pago",
     disabled: true,
-    href: '/profileAddPayment',
-
-  }
+    href: "/profileAddPayment",
+  },
 ];
+
 const colors = {
   primary: Colors.cs_primary,
   primary_dark: Colors.cs_primary_dark,
   white: Colors.cs_white,
 };
 
+const visible = ref(false);
+
+const months = Array.from({ length: 12 }, (_, i) => i + 1);
+
+const current_year = new Date().getFullYear();
+const years = Array.from({ length: 21 }, (_, i) => current_year + i);
+
+const card = {
+  owner: "",
+  number: "",
+  expiration_month: months[0],
+  expiration_year: years[0],
+  expiration_date: "",
+  cvv: "",
+};
+
+const state = reactive({ ...card });
+
+const rules = {
+  owner: {
+    required: withMessage("El nombre en la tarjeta es obligatorio", required),
+    regex: withMessage(
+      "El nombre en la tarjeta no puede contener caracteres especiales",
+      regex(
+        /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]*$/
+      )
+    ),
+  },
+  number: {
+    required: withMessage("El número de tarjeta es obligatorio", required),
+    regex: withMessage(
+      "El número de tarjeta debe ser un número",
+      regex(/^[0-9]*$/)
+    ),
+    minLength: withMessage(
+      "Numero de tarjeta debe tener 16 digitos",
+      minLength(16)
+    ),
+    maxLength: withMessage(
+      "Numero de tarjeta debe tener 16 digitos",
+      maxLength(16)
+    ),
+  },
+  expiration_month: {
+    required: withMessage("Mes de vencimiento obligatorio", required),
+  },
+  expiration_year: {
+    required: withMessage("Año de vencimiento obligatorio", required),
+  },
+  cvv: {
+    required: withMessage("CCV obligatorio", required),
+    regex: withMessage("CCV debe ser un número", regex(/^\d+$/)),
+    minLength: withMessage("CCV debe tener 3 digitos", minLength(3)),
+    maxLength: withMessage("CCV debe tener 3 digitos", maxLength(3)),
+  },
+};
+
+const v$ = useVuelidate(rules, state);
+
+const submitForm = () => {
+  v$.value.$touch();
+  if (v$.value.$error) return;
+
+  card.expiration_date = `${state.expiration_month}/${state.expiration_year}`;
+  // delete card.expiration_month;
+  // delete card.expiration_year;
+
+  alert(JSON.stringify(card));
+};
 </script>
