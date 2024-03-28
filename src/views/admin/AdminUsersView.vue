68b24d44c599baa377fc967daef03facc7129573
BetjaderOD <20213tn032@utez.edu.mx>
2024-03-27 18:38:31 -0600

    [ADD] Primera version de el componente header

--- src/views/admin/AdminUsersView.vue
+++ src/views/admin/AdminUsersView.vue
@@ -1,7 +1,10 @@
 <template>
-  <admin-layout> </admin-layout>
+  <admin-layout>
+    <admin-user-table-component />
+  </admin-layout>
 </template>
 
 <script setup>
 import AdminLayout from "../../layouts/admin/AdminLayout.vue";
+import AdminUserTableComponent from "../../components/admin/AdminUserTable.vue";
 </script>
