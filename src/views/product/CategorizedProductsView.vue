61b4d2e92015e94a3d899dcd65c52a0d8272f96a
CrisSoven <20213tn044@utez.edu.mx>
2024-03-28 14:23:41 -0600

    [MODIFY] reestructuración de diseño, implentación de breadcrumbs-component y redireccionamiento de rutas con :to + nombre de la ruta

--- src/views/product/CategorizedProductsView.vue
+++ src/views/product/CategorizedProductsView.vue
@@ -1,15 +1,15 @@
 <template>
   <default-layout>
     <subcategories-navbar-component />
-    <v-breadcrumbs :items="['Inicio', 'Hombres']" />
+    <breadcrumbs-component :items="items" />
     <v-container>
       <p class="text-h4 font-weight-medium mb-2">
-        {subcategory} para {category}
+        {{ $route.params.subcategory }} para {{ $route.params.category }}
       </p>
 
       <filter-products-component />
 
-      <div class="d-flex flex-wrap justify-center justify-lg-start mb-16">
+      <div class="d-flex flex-wrap justify-center justify-lg-start justify-md-start mb-16">
         <product-card-component v-for="i in 11" :key="i" />
       </div>
     </v-container>
@@ -20,4 +20,38 @@
 import DefaultLayout from "@/layouts/user/DefaultLayout.vue";
 import SubcategoriesNavbarComponent from "@/components/product/SubcategoriesNavbarComponent.vue";
 import ProductCardComponent from "@/components/common/ProductCardComponent.vue";
+import BreadcrumbsComponent from "@/components/common/BreadcrumbsComponent.vue";
+import FilterProductsComponent from "@/components/product/FilterProductsComponent.vue";
+
+const props = defineProps({
+  category: {
+    type: String,
+    default: "Categoría",
+  },
+  subcategory: {
+    type: String,
+    default: "Subcategoría",
+  },
+});
+
+const items = [
+  {
+    title: "Inicio",
+    to: { name: "Home" },
+  },
+  {
+    title: props.category,
+    to: {
+      name: "CategorizedProducts",
+      params: { category: props.category, subcategory: "todo" },
+    },
+  },
+  {
+    title: props.subcategory,
+    to: {
+      name: "CategorizedProducts",
+      params: { category: props.category, subcategory: "subcategory" },
+    },
+  },
+];
 </script>
--- src/views/product/SearchedProductsView.vue
+++ src/views/product/SearchedProductsView.vue
@@ -1,13 +1,17 @@
 <template>
   <default-layout>
-    <v-breadcrumbs :items="['Inicio', 'Hombres', 'Chamarras', 'ITEM NAME']" />
+    <breadcrumbs-component :items="items" />
     <v-container>
       <span>34 resultados para tu búsqueda</span>
-      <p class="text-h3 font-weight-medium my-2">"GORRA"</p>
+      <p class="text-h3 font-weight-medium my-2">
+        "{{ $route.params.query.toUpperCase() }}"
+      </p>
 
       <filter-products-component />
 
-      <div class="d-flex flex-wrap justify-center justify-lg-start mb-16">
+      <div
+        class="d-flex flex-wrap justify-center justify-lg-start justify-md-start mb-16"
+      >
         <product-card-component v-for="i in 10" :key="i" />
       </div>
     </v-container>
@@ -17,4 +21,39 @@
 <script setup>
 import DefaultLayout from "@/layouts/user/DefaultLayout.vue";
 import ProductCardComponent from "@/components/common/ProductCardComponent.vue";
+
+const props = defineProps({
+  category: {
+    type: String,
+    default: "Categoría",
+  },
+  subcategory: {
+    type: String,
+    default: "Subcategoría",
+  },
+});
+
+const items = [
+  {
+    title: "Inicio",
+    to: { name: "Home" },
+  },
+  {
+    title: props.category,
+    to: {
+      name: "CategorizedProducts",
+      params: { category: props.category, subcategory: "todo" },
+    },
+  },
+  {
+    title: "Subcategory",
+    to: {
+      name: "CategorizedProducts",
+      params: { category: props.category, subcategory: props.subcategory },
+    },
+  },
+  {
+    title: "Products list",
+  },
+];
 </script>
