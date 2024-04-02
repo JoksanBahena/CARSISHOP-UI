318c45ba1d452a6fb9cfa4a4172d445d3b48cf55
CrisSoven <20213tn044@utez.edu.mx>
2024-04-01 19:48:15 -0600

    [ADD] menú del perfil en el navbar

---
+++ src/components/common/ProfileMenuBtnComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-menu transition="slide-y-transition">
+    <template v-slot:activator="{ props }">
+      <v-btn
+        v-bind="props"
+        :style="{ color: colors.primary_dark }"
+        icon="mdi-account-outline"
+      />
+      <!-- :to="{ name: 'Login' }" -->
+    </template>
+    <v-list>
+      <user-profile-card-component />
+      <v-list-item
+        class="mt-3"
+        v-for="item in items"
+        :key="item.index"
+        :title="item.title"
+        :to="item.to"
+      />
+    </v-list>
+  </v-menu>
+</template>
+
+<script setup>
+import Colors from "@/utils/Colors.js";
+
+const colors = {
+  primary_dark: Colors.cs_primary_dark,
+};
+
+const items = [
+  {
+    title: "Mi perfil",
+    to: { name: "Profile" },
+  },
+];
+</script>
