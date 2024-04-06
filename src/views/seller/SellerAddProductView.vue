060e6d4f12b7199b1781f967c2b6c3c2e5f13232
CrisSoven <20213tn044@utez.edu.mx>
2024-04-06 03:33:24 -0600

    [MODIFY] rediseño y validación de tallas

--- src/views/seller/SellerAddProductView.vue
+++ src/views/seller/SellerAddProductView.vue
@@ -17,6 +17,7 @@
               placeholder="Nombre"
               prepend-inner-icon="mdi-package-variant-closed"
               variant="outlined"
+              counter="100"
               @blur="v$.name.$touch"
               @input="v$.name.$touch"
               :error-messages="v$.name.$errors.map((e) => e.$message)"
@@ -33,110 +34,119 @@
               prepend-inner-icon="mdi-package-variant-closed"
               variant="outlined"
               auto-grow
+              counter="200"
               @blur="v$.description.$touch"
               @input="v$.description.$touch"
               :error-messages="v$.description.$errors.map((e) => e.$message)"
             />
           </div>
+
           <div class="mb-4">
-            <div class="text-subtitle-1 font-weight-medium">
-              Precio del producto
-            </div>
-            <v-text-field
-              v-model="state.price"
-              density="compact"
-              placeholder="$0.00"
-              prepend-inner-icon="mdi-currency-usd"
+            <v-card
               variant="outlined"
-              type="number"
-              hide-spin-buttons
-              @blur="v$.price.$touch"
-              @input="v$.price.$touch"
-              :error-messages="v$.price.$errors.map((e) => e.$message)"
-            />
+              :color="
+                v$.stock.$errors.length > 0 ? colors.red : 'grey-lighten-1'
+              "
+            >
+              <v-expansion-panels flat>
+                <v-expansion-panel>
+                  <v-expansion-panel-title>
+                    <div class="d-flex flex-row align-center">
+                      <div class="text-subtitle-1 font-weight-medium">
+                        Tallas
+                      </div>
+                      <div class="ml-2">
+                        ({{
+                          selected_sizes.length > 0
+                            ? selected_sizes.join(", ")
+                            : "Define al menos una talla"
+                        }})
+                      </div>
+                    </div>
+
+                    <template v-slot:actions>
+                      <v-icon
+                        :color="v$.stock.$errors.length > 0 ? colors.red : ''"
+                      >
+                        {{
+                          v$.stock.$errors.length > 0
+                            ? "mdi-alert-circle-outline"
+                            : "mdi-chevron-down"
+                        }}
+                      </v-icon>
+                    </template>
+                  </v-expansion-panel-title>
+
+                  <v-expansion-panel-text>
+                    <v-chip-group
+                      v-model="selected_sizes"
+                      variant="outlined"
+                      selected-class="text-white bg-indigo-darken-4"
+                      multiple
+                    >
+                      <v-chip
+                        v-for="(size, index) in sizes"
+                        :key="index"
+                        :value="size"
+                      >
+                        {{ size }}
+                      </v-chip>
+                    </v-chip-group>
+
+                    <template v-for="(size, index) in state.stock" :key="index">
+                      <v-row>
+                        <v-col cols="6" lg="4" md="4">
+                          <div class="text-subtitle-1 font-weight-medium">
+                            Stock {{ size.size }}
+                          </div>
+                          <v-text-field
+                            v-model="size.quantity"
+                            density="compact"
+                            placeholder="0"
+                            :prepend-inner-icon="
+                              'mdi-size-' + size.size.toLowerCase()
+                            "
+                            variant="outlined"
+                            type="number"
+                            autofocus
+                            @blur="v$.stock.$touch"
+                            @input="v$.stock.$touch"
+                            :error-messages="
+                              v$.stock.$each.$response.$errors[
+                                index
+                              ].quantity.map((e) => e.$message)
+                            "
+                          />
+                        </v-col>
+                        <br />
+                        <v-col cols="6" lg="8" md="8">
+                          <div class="text-subtitle-1 font-weight-medium">
+                            Precio
+                          </div>
+                          <v-text-field
+                            v-model="size.price"
+                            density="compact"
+                            placeholder="$0.00"
+                            prepend-inner-icon="mdi-currency-usd"
+                            variant="outlined"
+                            type="number"
+                            hide-spin-buttons
+                            @blur="v$.stock.$touch"
+                            @input="v$.stock.$touch"
+                            :error-messages="
+                              v$.stock.$each.$response.$errors[index].price.map(
+                                (e) => e.$message
+                              )
+                            "
+                          />
+                        </v-col>
+                      </v-row>
+                    </template>
+                  </v-expansion-panel-text>
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </v-card>
           </div>
-          <v-row>
-            <v-col cols="6" lg md>
-              <div class="mb-4">
-                <div class="text-subtitle-1 font-weight-medium">Stock XS</div>
-                <v-text-field
-                  v-model="state.stock.xs"
-                  density="compact"
-                  placeholder="0"
-                  prepend-inner-icon="mdi-size-xs"
-                  variant="outlined"
-                  type="number"
-                  @blur="v$.stock.xs.$touch"
-                  @input="v$.stock.xs.$touch"
-                  :error-messages="v$.stock.xs.$errors.map((e) => e.$message)"
-                />
-              </div>
-            </v-col>
-            <v-col cols="6" lg md>
-              <div class="mb-4">
-                <div class="text-subtitle-1 font-weight-medium">Stock S</div>
-                <v-text-field
-                  v-model="state.stock.s"
-                  density="compact"
-                  placeholder="0"
-                  prepend-inner-icon="mdi-size-s"
-                  variant="outlined"
-                  type="number"
-                  @blur="v$.stock.s.$touch"
-                  @input="v$.stock.s.$touch"
-                  :error-messages="v$.stock.s.$errors.map((e) => e.$message)"
-                />
-              </div>
-            </v-col>
-            <v-col cols="6" lg md>
-              <div class="mb-4">
-                <div class="text-subtitle-1 font-weight-medium">Stock M</div>
-                <v-text-field
-                  v-model="state.stock.m"
-                  density="compact"
-                  placeholder="0"
-                  prepend-inner-icon="mdi-size-m"
-                  variant="outlined"
-                  type="number"
-                  @blur="v$.stock.m.$touch"
-                  @input="v$.stock.m.$touch"
-                  :error-messages="v$.stock.m.$errors.map((e) => e.$message)"
-                />
-              </div>
-            </v-col>
-            <v-col cols="6" lg md>
-              <div class="mb-4">
-                <div class="text-subtitle-1 font-weight-medium">Stock L</div>
-                <v-text-field
-                  v-model="state.stock.l"
-                  density="compact"
-                  placeholder="0"
-                  prepend-inner-icon="mdi-size-l"
-                  variant="outlined"
-                  type="number"
-                  @blur="v$.stock.l.$touch"
-                  @input="v$.stock.l.$touch"
-                  :error-messages="v$.stock.l.$errors.map((e) => e.$message)"
-                />
-              </div>
-            </v-col>
-            <v-col cols="6" lg md>
-              <div class="mb-4">
-                <div class="text-subtitle-1 font-weight-medium">Stock XL</div>
-                <v-text-field
-                  v-model="state.stock.xl"
-                  density="compact"
-                  placeholder="0"
-                  prepend-inner-icon="mdi-size-xl"
-                  variant="outlined"
-                  type="number"
-                  @blur="v$.stock.xl.$touch"
-                  @input="v$.stock.xl.$touch"
-                  :error-messages="v$.stock.xl.$errors.map((e) => e.$message)"
-                />
-              </div>
-            </v-col>
-          </v-row>
           <v-row>
             <v-col>
               <div class="mb-4">
@@ -177,22 +187,40 @@
           </v-row>
         </v-col>
         <v-col cols="12" lg="5" md="5">
-          <div class="text-subtitle-1 font-weight-medium">Imagenes</div>
+          <div class="text-subtitle-1 font-weight-medium">Imágenes</div>
           <v-file-input
+            v-model="state.image_urls.value"
             density="compact"
             type="file"
             accept="image/png, image/jpeg, image/bmp"
             variant="outlined"
             prepend-icon="mdi-camera-outline"
+            clear-icon="mdi-close"
             show-size
             multiple
             chips
-            clearable
             @change="onFileChange"
-          />
+            @blur="v$.image_urls.$touch"
+            @input="v$.image_urls.$touch"
+            :error-messages="v$.image_urls.$errors.map((e) => e.$message)"
+          >
+            <template v-slot:selection="{ fileNames }">
+              <template v-for="(fileName, index) in fileNames" :key="fileName">
+                <v-chip
+                  v-if="index < max_images"
+                  class="me-2"
+                  color="deep-purple-accent-4"
+                  size="small"
+                  label
+                >
+                  {{ fileName }}
+                </v-chip>
+              </template>
+            </template>
+          </v-file-input>
           <div
-            v-if="image_urls.length > 0"
-            class="d-flex flex-column pb-6"
+            v-if="state.image_urls.length > 0"
+            class="d-flex flex-column pb-6 mt-4"
             :style="{ border: '2px dashed' + colors.gray }"
           >
             <div class="text-subtitle-1 font-weight-medium ma-3 mr-auto">
@@ -200,7 +228,7 @@
             </div>
             <v-row>
               <v-col
-                v-for="(url, index) in image_urls"
+                v-for="(url, index) in state.image_urls"
                 :key="index"
                 draggable
                 @dragstart="handleDragStart($event, index)"
@@ -208,7 +236,7 @@
                 @drop="handleDrop($event, index)"
                 cols="12"
                 lg="5"
-                md="5"
+                md="8"
               >
                 <div class="d-flex align-center mx-12">
                   <p class="mr-1">{{ index + 1 }}.</p>
@@ -220,7 +248,7 @@
             </v-row>
           </div>
           <div
-            v-if="image_urls.length > 0"
+            v-if="state.image_urls.length > 0"
             class="d-flex align-center justify-center py-2 mt-6"
             :style="{ border: '2px dashed' + colors.gray }"
             @drop="removeImage($event)"
@@ -263,13 +291,18 @@
 </template>
 
 <script setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, watch } from "vue";
 import Colors from "@/utils/Colors.js";
-import Swal from "sweetalert2";
 import { useVuelidate } from "@vuelidate/core";
-import { required, decimal, maxLength, helpers } from "@vuelidate/validators";
+import {
+  required,
+  decimal,
+  minLength,
+  maxLength,
+  helpers,
+} from "@vuelidate/validators";
 
-const { withMessage, regex } = helpers;
+const { withMessage, forEach, regex } = helpers;
 
 const items = [
   {
@@ -297,6 +330,7 @@ const colors = {
   primary: Colors.cs_primary,
   primary_dark: Colors.cs_primary_dark,
   gray: Colors.cs_opacity_gray,
+  red: Colors.cs_red,
 };
 
 const categories = ["Hombre", "Mujer", "Niño", "Niña"];
@@ -312,28 +346,29 @@ const subcategories = [
 const product = {
   name: "",
   description: "",
-  price: "",
-  stock: {
-    xs: null,
-    s: null,
-    m: null,
-    l: null,
-    xl: null,
-  },
   category: null,
   subcategory: null,
+  stock: [],
+  image_urls: [],
 };
 
-const image_urls = ref([]);
+const max_size = 2;
+const max_images = 5;
 
 const state = reactive({ ...product });
+
+const imgSizeValidate = () => {
+  return (value) => {
+    return value.value.every((image) => image.size / 1024 / 1024 <= max_size);
+  };
+};
 
 const rules = {
   name: {
     required: withMessage("El nombre del producto es obligatorio", required),
     maxLength: withMessage(
-      "El nombre del producto no debe tener más de 30 carácteres",
-      maxLength(50)
+      "El nombre del producto no debe tener más de 100 carácteres",
+      maxLength(100)
     ),
   },
   description: {
@@ -342,42 +377,9 @@ const rules = {
       required
     ),
     maxLength: withMessage(
-      "La descripción del producto no debe tener más de 500 carácteres",
-      maxLength(500)
+      "La descripción del producto no debe tener más de 200 carácteres",
+      maxLength(200)
     ),
-  },
-  price: {
-    required: withMessage("El precio del producto es obligatorio", required),
-    decimal: withMessage(
-      "El precio del producto debe ser un valor valido",
-      decimal
-    ),
-    regex: withMessage(
-      "El precio del producto debe ser un valor valido",
-      regex(/^\d+(\.\d{1,2})?$/)
-    ),
-  },
-  stock: {
-    xs: {
-      required: withMessage("El stock XS es obligatorio", required),
-      regex: withMessage("El stock debe ser valido", regex(/^\d+$/)),
-    },
-    s: {
-      required: withMessage("El stock S es obligatorio", required),
-      regex: withMessage("El stock debe ser valido", regex(/^\d+$/)),
-    },
-    m: {
-      required: withMessage("El stock M es obligatorio", required),
-      regex: withMessage("El stock debe ser valido", regex(/^\d+$/)),
-    },
-    l: {
-      required: withMessage("El stock L es obligatorio", required),
-      regex: withMessage("El stock debe ser valido", regex(/^\d+$/)),
-    },
-    xl: {
-      required: withMessage("El stock XL es obligatorio", required),
-      regex: withMessage("El stock debe ser valido", regex(/^\d+$/)),
-    },
   },
   category: {
     required: withMessage("La categoría es obligatoria", required),
@@ -385,77 +387,76 @@ const rules = {
   subcategory: {
     required: withMessage("La subcategoría es obligatoria", required),
   },
+  stock: {
+    required,
+    minLength: minLength(1),
+    $each: forEach({
+      quantity: {
+        required: withMessage("El stock es obligatorio", required),
+        regex: withMessage("El stock debe ser valido", regex(/^\d+$/)),
+      },
+      price: {
+        required: withMessage("El precio es obligatorio", required),
+        decimal: withMessage("El precio debe valido", decimal),
+        regex: withMessage(
+          "El precio debe tener máximo 2 decimales",
+          regex(/^\d+(\.\d{1,2})?$/)
+        ),
+      },
+    }),
+  },
+  image_urls: {
+    required: withMessage("Mínimo 2 imágenes del producto", required),
+    imgSizeValidate: withMessage(
+      `El tamaño de la imagen no debe exceder ${max_size}MB`,
+      imgSizeValidate()
+    ),
+    minLength: withMessage("Mínimo 2 imágenes del producto", minLength(2)),
+    maxLength: withMessage(
+      `Máximo ${max_images} imágenes del producto`,
+      maxLength(4)
+    ),
+  },
 };
 
 const v$ = useVuelidate(rules, state);
 
+const sizes = ["XS", "S", "M", "L", "XL"];
+
+const selected_sizes = ref([]);
+
 const submitForm = () => {
   v$.value.$touch();
-  if (v$.value.$error) return;
 
-  if (image_urls.value.length < 2) {
-    Swal.fire({
-      title: "Error!",
-      text: "Por favor, sube al menos 2 imágenes",
-      icon: "error",
-      confirmButtonText: "Ok",
-    });
-    return;
-  }
+  console.log(v$.value.stock);
+
+  if (v$.value.$error) return;
 
   alert(JSON.stringify(state));
 };
 
 const onFileChange = (e) => {
   const files = e.target.files;
-  const allowedTypes = ["image/png", "image/jpeg", "image/bmp"];
-  const maxSize = 10_000_000;
-  const maxImages = 5;
+  const remaining_space = max_images - state.image_urls.length;
+  const files_add = Math.min(remaining_space, files.length);
 
-  if (image_urls.value.length + files.length > maxImages) {
-    Swal.fire({
-      title: "Error!",
-      text: "Solo se permiten hasta un máximo de 5 imágenes",
-      icon: "error",
-      confirmButtonText: "Ok",
-    });
-    return;
-  }
-
-  for (let i = 0; i < files.length; i++) {
+  for (let i = 0; i < files_add; i++) {
     const file = files[i];
+    if (file.size / 1024 / 1024 <= max_size) {
+      const reader = new FileReader();
 
-    if (!allowedTypes.includes(file.type)) {
-      Swal.fire({
-        title: "Error!",
-        text: "Solo se permiten archivos de imagen en formato PNG, JPEG o BMP",
-        icon: "error",
-        confirmButtonText: "Ok",
-      });
-      return;
+      reader.onload = (event) => {
+        state.image_urls.push(event.target.result);
+      };
+
+      reader.readAsDataURL(file);
     }
-
-    if (file.size > maxSize) {
-      Swal.fire({
-        title: "Error!",
-        text: "La imagen no debe pesar más de 10MB",
-        icon: "error",
-        confirmButtonText: "Ok",
-      });
-      return;
-    }
-
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      image_urls.value.push(e.target.result);
-    };
-    reader.readAsDataURL(file);
   }
 };
 
 const removeImage = (event) => {
   const index = event.dataTransfer.getData("text/plain");
-  image_urls.value.splice(index, 1);
+  state.image_urls.splice(index, 1);
 };
 
 const handleDragStart = (event, index) => {
@@ -475,12 +476,37 @@ const handleDrop = (event, dropIndex) => {
     return;
   }
 
-  const movedImageUrl = image_urls.value[draggedIndex];
-  image_urls.value.splice(draggedIndex, 1);
-  image_urls.value.splice(dropIndex, 0, movedImageUrl);
+  const movedImageUrl = state.image_urls[draggedIndex];
+  state.image_urls.splice(draggedIndex, 1);
+  state.image_urls.splice(dropIndex, 0, movedImageUrl);
 };
 
 const clear = () => {
-  image_urls.value = [];
+  v$.value.$reset();
+  state.name = "";
+  state.description = "";
+  state.category = null;
+  state.subcategory = null;
+  state.stock = [];
+  state.image_urls = [];
+  selected_sizes.value = [];
 };
+
+watch(selected_sizes, (value, old_value) => {
+  const deselected_sizes = old_value.filter((size) => !value.includes(size));
+  state.stock = state.stock.filter(
+    (item) => !deselected_sizes.includes(item.size)
+  );
+
+  const existingSizes = state.stock.map((item) => item.size);
+  const newSizes = value.filter((size) => !existingSizes.includes(size));
+
+  const newStock = newSizes.map((size) => ({
+    size,
+    quantity: 0,
+    price: "",
+  }));
+
+  state.stock = [...state.stock, ...newStock];
+});
 </script>
